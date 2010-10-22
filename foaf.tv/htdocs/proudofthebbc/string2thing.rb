d265fb13ca6efc872ca66f6f72397555e47768f8
Dan Brickley <danbri@danbri.org>
2010-10-22 21:00:06 +0200

    mess

--- foaf.tv/htdocs/proudofthebbc/string2thing.rb
+++ foaf.tv/htdocs/proudofthebbc/string2thing.rb
@@ -5,7 +5,9 @@ require 'rdf' # http://rdf.rubyforge.org/
 
 require 'rdf/ntriples'
 require 'rdf/raptor'
+require 'open-uri' 
 
+#print open('http://www.domain.com/path/page.rhtml').read
 
 # puts RDF::Raptor.available?         #=> true
 # puts RDF::Raptor.version            #=> "1.4.21"
@@ -27,8 +29,11 @@ $stdout.sync = true
 
 # curl -s -e http://danbri.org/  'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=bbc%20site:wikipedia.org'
 
-wiki_qs = "curl -s -e http://danbri.org/  'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=zzzzz%20site:wikipedia.org'"
- bbc_qs = "curl -s -e http://danbri.org/  'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=zzzzz%20site:www.bbc.co.uk/programmes/'"
+#wiki_qs = "curl -s -e http://danbri.org/  'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=zzzzz%20site:wikipedia.org'"
+# bbc_qs = "curl -s -e http://danbri.org/  'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=zzzzz%20site:www.bbc.co.uk/programmes/'"
+
+wiki_qs = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=zzzzz%20site:wikipedia.org"
+ bbc_qs = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=zzzzz%20site:www.bbc.co.uk/programmes/"
 
 
 # results: {"responseData": {"results":[{"GsearchResultClass":"GwebSearch","unescapedUrl": ...
@@ -46,14 +51,16 @@ f.each do |item|
   esc = item.gsub(/\s+/,'%20')
   esc.gsub!(/&/,'')
   esc.gsub!(/'/,'')
+  esc.gsub!(/\'/,'')
 
   puts "# #{item}"
 
   # Find Wikipedia links
   # Later we can find dbpedia.org links for metadata
   #
-  wres = ` #{ wiki_qs.gsub(/zzzzz/, esc) } ` # umm
+  wres = open(wiki_qs.gsub(/zzzzz/, esc) ).read 
 
+#  puts "Got #{item}\n\n"
 
   wiki_feeling_lucky = JSON.parse(wres)['responseData']['results'][0]['unescapedUrl']
   wiki_feeling_lucky =~ /wikipedia\.org\/wiki\/(.*)/
@@ -72,7 +79,8 @@ f.each do |item|
   # Find BBC /programmes/ links
   # (later, we can append .rdf to get metadata)
   #
-  bres =` #{ bbc_qs.gsub(/zzzzz/, esc) } ` 
+#  bres =` #{ bbc_qs.gsub(/zzzzz/, esc) } ` 
+  bres = open(bbc_qs.gsub(/zzzzz/, esc) ).read 
   bbc_feeling_lucky = JSON.parse(bres)['responseData']['results'][0]['unescapedUrl']
   bbc_feeling_lucky =~ /bbc\.co\.uk\/programmes\/(.*)/
   bid = $1
