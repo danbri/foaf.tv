171e8e70b2e5c80b7066ec88490c31915f7bb993
Dan Brickley <danbri@danbri.org>
2010-10-22 14:30:55 +0200

    working

---
+++ foaf.tv/htdocs/proudofthebbc/string2thing.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby -rubygems
+
+require 'json'
+
+# Takes a list of BBC-related things and gets some Wikipedia URLs
+# Source list is from http://mitchbenn.com/
+
+# curl -s -e http://danbri.org/  'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=bbc%20site:wikipedia.org'
+
+wiki_qs = "curl -s -e http://danbri.org/  'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=zzzzz%20site:wikipedia.org'"
+ bbc_qs = "curl -s -e http://danbri.org/  'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=zzzzz%20site:www.bbc.co.uk/programmes/'"
+
+
+# results: {"responseData": {"results":[{"GsearchResultClass":"GwebSearch","unescapedUrl": ...
+
+
+f=File.open('lyrics-list.txt').read
+f.each do |item|
+  item.chomp!
+  next unless (item =~ /\w/) 
+  next if (item =~ /^#/) 
+  next if (item =~ /^\s\*/) 
+  esc = item.gsub(/\s+/,'%20')
+  esc.gsub!(/&/,'')
+  esc.gsub!(/'/,'')
+
+  # Find Wikipedia links
+  # Later we can find dbpedia.org links for metadata
+  #
+  wres = ` #{ wiki_qs.gsub(/zzzzz/, esc) } ` # umm
+  wiki_feeling_lucky = JSON.parse(wres)['responseData']['results'][0]['unescapedUrl']
+  puts "#{  wiki_feeling_lucky}\t# Wiki";
+
+  # Find BBC /programmes/ links
+  # (later, we can append .rdf to get metadata)
+  #
+  bres =` #{ bbc_qs.gsub(/zzzzz/, esc) } ` 
+  bbc_feeling_lucky = JSON.parse(bres)['responseData']['results'][0]['unescapedUrl']
+  puts "Programmes:\t#{  bbc_feeling_lucky}\t# Wiki ";
+
+
+end
